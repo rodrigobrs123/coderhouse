11d31ada2ac144179e4477b729099b7614ec450e
Rodrigo Barreto <rodrigobrs111@gmail.com>
2024-03-24 23:12:47 -0300

    Loja Virtual Rodrigo Barreto -> Menu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import NavbarBootstrap from './components/js/NavBarBS';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
